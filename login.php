9664b19a65ccaf8830903571567e42930dba5ff0
Tosiek0 <nieantek111@icloud.com>
2026-04-16 21:50:17 +0200

    name update

--- login.php
+++ login.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $_SESSION['id'] = $user['id'];
             $_SESSION['username'] = $user['username'];
 
-            header("Location: index.html");
+            header("Location: home.html");
             exit;
         } else {
             $error = "Nieprawidłowe hasło";
