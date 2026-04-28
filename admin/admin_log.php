3a43c9186f7849e46db68376a07bf7d3c3bc39d2
Tosiek0 <nieantek111@icloud.com>
2026-04-28 10:56:17 +0200

    name update

--- admin/admin_log.php
+++ admin/admin_log.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $_SESSION['id'] = $user['id'];
             $_SESSION['username'] = $user['username'];
 
-            header("Location: admin.php");
+            header("Location: admin_home.html");
             exit;
         } else {
             $error = "Nieprawidłowe hasło";
